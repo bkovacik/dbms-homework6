8b00dfc9da0c4db5b8b8d475dbbdde5107747dba
Brian Kovacik <varnishedgreen@aim.com>
2016-04-18 18:34:21 -0400

    added * parsing

--- hw6.py
+++ hw6.py
@@ -2,19 +2,27 @@ import sys
 import csv
 #import queryDB1 from queryDB1
 #import queryDB2 from queryDB2
-#import queryDB3 from queryDB3
-
+from queryDB3 import queryDB3 
 assert len(sys.argv) == 2
 inputFileName = sys.argv[1]
 
 actorIdRoot = "actor_id_idx/root.txt"
-movieRolesRoot = "movieroles_ma_idx.txt"
+movieRolesRoot = "movieroles_ma_idx/root.txt"
 
-with open inputFileName as inputFile:
-	linereader = csv.reader(inputFile, delimiter = "\n")
-	for line_ in linereader:
-		line = line_[0].split(",")
-		[movieIdLow, movieIdHigh, actorIdLow, actorIdHigh] = line
+with open(inputFileName) as inputFile:
+  linereader = csv.reader(inputFile, delimiter = "\n")
+  for line_ in linereader:
+    line = line_[0].split(",")
+    [movieIdLow, movieIdHigh, actorIdLow, actorIdHigh] = line
+    if movieIdLow == '*':
+      movieIdLow = -1
+    if actorIdLow == '*':
+      actorIdLow = -1
+    if movieIdHigh == '*':
+      movieIdHigh = sys.maxint
+    if actorIdHigh == '*':
+      actorIdHigh = sys.maxint
+
 		#queryDB1(actorIdRoot, movieRolesRoot, movieIdLow, movieIdHigh, actorIdLow, actorIdHigh)
 		#queryDB2(actorIdRoot, movieRolesRoot, movieIdLow, movieIdHigh, actorIdLow, actorIdHigh)
-		#queryDB3(actorIdRoot, movieRolesRoot, movieIdLow, movieIdHigh, actorIdLow, actorIdHigh)
+    queryDB3(int(movieIdLow), int(movieIdHigh), int(actorIdLow), int(actorIdHigh))
